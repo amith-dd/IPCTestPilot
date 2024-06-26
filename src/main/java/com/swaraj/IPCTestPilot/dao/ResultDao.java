9eda773cb6bb66dd4af8d49309eb3971c390fb28
Ajaydevagan <devagan9344@gmail.com>
2024-06-26 18:59:34 +0530

    2 more method added in resultDao

--- src/main/java/com/swaraj/IPCTestPilot/dao/ResultDao.java
+++ src/main/java/com/swaraj/IPCTestPilot/dao/ResultDao.java
@@ -1,5 +1,50 @@
 package com.swaraj.IPCTestPilot.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+
+import com.swaraj.IPCTestPilot.dto.Result;
+import com.swaraj.IPCTestPilot.repo.ResultRepo;
+
 public class ResultDao {
+	
+	ResultRepo repo;
+	
+	public Result saveResult(Result result) {
+		return repo.save(result);
+	}
+	
+	public Result findResult(int studentId) {
+		Optional<Result> opresult = repo.findById(studentId);
+		if(opresult.isPresent()) {
+			return opresult.get();
+		}
+		return null;
+	}
+	
+	public Result deleteResult(int studentId) {
+		Result result = findResult(studentId);
+		if(result!=null) {
+			repo.delete(result);
+			return result;
+		}
+		return null;
+	}
+	
+	public List<Result> findAll()
+	{
+		return repo.findAll();
+	}
+	
+	public List<Result> findAllById(int studentId)
+	{
+		List<Result> list=repo.findAllById(studentId);
+		if(list!=null)
+		{
+			return list;
+		}
+		return null;
+	}
 
 }
