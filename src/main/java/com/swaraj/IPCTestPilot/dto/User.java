77c27dc525bf4165e54006c25ff02d488e6a500d
RubanvasanthK <159932886+RubanvasanthK@users.noreply.github.com>
2024-06-27 19:31:21 +0530

    trainer and depended files are changed into User

---
+++ src/main/java/com/swaraj/IPCTestPilot/dto/User.java
@@ -0,0 +1,68 @@
+package com.swaraj.IPCTestPilot.dto;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Component
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private long userContact;
+	private String userEmail;
+	private String userPassword;
+	@ElementCollection
+	private List<Integer> userCourseIds;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public long getUserContact() {
+		return userContact;
+	}
+	public void setUserContact(long userContact) {
+		this.userContact = userContact;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public List<Integer> getUserCourseIds() {
+		return userCourseIds;
+	}
+	public void setUserCourseIds(List<Integer> userCourseIds) {
+		this.userCourseIds = userCourseIds;
+	}
+	
+	
+	
+	
+	
+}
