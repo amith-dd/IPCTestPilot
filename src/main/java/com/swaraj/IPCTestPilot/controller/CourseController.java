e25935f7c794dd7f0653f9bfa84dbaa29a0f1b5a
amith-dd <amithdd0@gmail.com>
2024-08-09 10:57:12 +0530

    delete course, update course added in Course layer

--- src/main/java/com/swaraj/IPCTestPilot/controller/CourseController.java
+++ src/main/java/com/swaraj/IPCTestPilot/controller/CourseController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +43,18 @@ public class CourseController {
 	@GetMapping("/{courseId}")
 	public ResponseEntity<ResponseStructure<List<String>>> getAllSubjectByCourseId(@PathVariable int courseId){
 		return service.getAllSubjectByCourseId(courseId);
+	}
+	
+	@Operation(summary = "delete By Course ID")
+	@DeleteMapping("/{courseId}")
+	public ResponseEntity<ResponseStructure<Course>> deleteCourseById(@PathVariable int courseId){
+		return service.deleteCource(courseId);
+	}
+	
+	@Operation(summary = "update Course By Course ID")
+	@PutMapping
+	public ResponseEntity<ResponseStructure<Course>> deleteCourseById(@RequestBody Course course ,@RequestParam int courseId){
+		return service.updateCource(course, courseId);
 	}
 	
 }
--- src/main/java/com/swaraj/IPCTestPilot/service/CourseService.java
+++ src/main/java/com/swaraj/IPCTestPilot/service/CourseService.java
@@ -20,62 +20,87 @@ public class CourseService {
 
 	@Autowired
 	CourseDao dao;
-	
+
 	/*
 	 * save course with subject id's
 	 */
 	public ResponseEntity<ResponseStructure<Course>> saveCourse(String courseName, List<Integer> subjectids) {
-		
+
 		Course course = new Course();
 		course.setCourseName(courseName);
-        course.setSubjectIds(subjectids);
-		
+		course.setSubjectIds(subjectids);
+
 		Course savedCourse = dao.saveCourse(course);
 		if (savedCourse != null) {
 			ResponseStructure<Course> structure = new ResponseStructure<Course>();
 			structure.setData(savedCourse);
 			structure.setMessage("course saved");
-			structure.setStatus(HttpStatus.CREATED.value());//gives the code
+			structure.setStatus(HttpStatus.CREATED.value());// gives the code
 
 			return new ResponseEntity<ResponseStructure<Course>>(structure, HttpStatus.CREATED);
 		}
 		throw new CourseNotSavedException("course save failed");
 	}
-	
-	
-	
-	public ResponseEntity<ResponseStructure<List<Course>>> getAllCourses(){
+
+	public ResponseEntity<ResponseStructure<List<Course>>> getAllCourses() {
 		List<Course> courses = dao.getAll();
-		if(courses!=null) {
+		if (courses != null) {
 			ResponseStructure<List<Course>> structure = new ResponseStructure<List<Course>>();
 			structure.setData(courses);
 			structure.setMessage("data found for all courses");
 			structure.setStatus(HttpStatus.FOUND.value());
-			return new ResponseEntity<ResponseStructure<List<Course>>>(structure,HttpStatus.FOUND);
+			return new ResponseEntity<ResponseStructure<List<Course>>>(structure, HttpStatus.FOUND);
 		}
 		throw new CourseNotFoundException("course not found");
 	}
-	
+
 	/*
 	 * get all subjects with given course id
 	 */
-	
-	public ResponseEntity<ResponseStructure<List<String>>> getAllSubjectByCourseId(int courseId){
+
+	public ResponseEntity<ResponseStructure<List<String>>> getAllSubjectByCourseId(int courseId) {
 		Course course = dao.findCourse(courseId);
-		if(course!=null) {
+		if (course != null) {
 			List<String> subjects = new ArrayList<String>();
 			List<Integer> subjectsIds = course.getSubjectIds();
-			for(int i : subjectsIds) {
+			for (int i : subjectsIds) {
 				subjects.add(Subject.values()[i].name());
 			}
 			ResponseStructure<List<String>> structure = new ResponseStructure<List<String>>();
 			structure.setData(subjects);
 			structure.setMessage("subjects found for given course Id");
 			structure.setStatus(HttpStatus.FOUND.value());
-			return new ResponseEntity<ResponseStructure<List<String>>>(structure,HttpStatus.FOUND);
+			return new ResponseEntity<ResponseStructure<List<String>>>(structure, HttpStatus.FOUND);
 		}
 		throw new CourseNotFoundException("course not found with the given ID");
 	}
-	
+
+	public ResponseEntity<ResponseStructure<Course>> deleteCource(int courseId) {
+		Course deletedCourse = dao.deleteCourse(courseId);
+		if (deletedCourse != null) {
+			ResponseStructure<Course> structure = new ResponseStructure<Course>();
+			structure.setData(deletedCourse);
+			structure.setMessage("data found for all courses");
+			structure.setStatus(HttpStatus.OK.value());
+			return new ResponseEntity<ResponseStructure<Course>>(structure, HttpStatus.OK);
+		}
+		throw new CourseNotFoundException("course not found with the given ID");
+	}
+
+	public ResponseEntity<ResponseStructure<Course>> updateCource(Course course, int courseId) {
+		Course dCoursetobeUpdated = dao.findCourse(courseId);
+		if (dCoursetobeUpdated != null) {
+			if (course != null && course.getSubjectIds()!=null) {
+				course.setCourseId(courseId);
+				ResponseStructure<Course> structure = new ResponseStructure<Course>();
+				structure.setData(dao.updateCourse(course, courseId));
+				structure.setMessage("course updated");
+				structure.setStatus(HttpStatus.OK.value());
+				return new ResponseEntity<ResponseStructure<Course>>(structure, HttpStatus.OK);
+			}
+			throw new CourseNotSavedException("course not updated because of internal error");
+		}
+		throw new CourseNotFoundException("course not found with the given ID");
+	}
 
 }
